b4f871efda17bd1114c7c83a9c1b9809130b5fa2
luana2322 <nguyenbaoluanthai@gmail.com>
2024-07-27 20:25:14 +0700

    change model property

--- user/models.py
+++ user/models.py
@@ -6,6 +6,7 @@ class User(models.Model):
   lastname = models.CharField(max_length=255)
   gpa = models.FloatField()
   email = models.EmailField()
+  gender=models.CharField(max_length=225)
 
 def __str__(self):
     return self.name
